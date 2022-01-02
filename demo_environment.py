b4a28166e6a0783ad76b98d71b3c304eb21ff170
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-01-02 16:08:55 +0800

    Update demo_environment.py

--- demo_environment.py
+++ demo_environment.py
@@ -1,17 +1,19 @@
-from simulator import Vehicle, Environment, Park, initFrame, resetEnv
+from simulator import Vehicle, Environment, Park, initFrame, resetEnvVel
 import matplotlib.pyplot as plt
 import torch
 
 if __name__ == '__main__':
-    environment = Environment(Vehicle(0, 0, 0, 0, 0), Park())
+    environment = Environment(Vehicle(0, 0, 0, 0, 0), Park(), reset_fn=resetEnvVel)
     environment.seed(0)
     environment.reset()
     fig, ax = environment.InitFrame()
 
     while True:
-        # environment.render(fig, ax, mode='train')
+        environment.render(fig, ax, mode='train')
         dummy_observation = environment.observation
-        dummy_action = torch.randn(32)
+        dummy_action = torch.randn(16)
+        dummy_action[0] = 1
+        dummy_action[15] = 1
         obs, rwd, is_done, info = environment.step(dummy_action)
         distances = environment.vehicle.GetAllRadarDistance(environment.park.segAll)
         print("distance: ", distances.squeeze())

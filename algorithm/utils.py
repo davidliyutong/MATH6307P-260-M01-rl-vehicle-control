5e41b2a8df140c80214bc93f5f649c56c424ce16
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-01-02 16:09:35 +0800

    Format file

--- algorithm/utils.py
+++ algorithm/utils.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 import numpy as np
 
 
+
 def identity(x):
     return x
 
@@ -47,7 +48,7 @@ def to_tensor_var(x, use_cuda=True, dtype="float"):
 def agg_double_list(l):
     # l: [ [...], [...], [...] ]
     # l_i: result of each step in the i-th episode
-    s = [np.sum(np.array(l_i), 0) for l_i in l]
+    s = [np.sum(np.array(th.cat(l_i).cpu()), 0) for l_i in l]
     s_mu = np.mean(np.array(s), 0)
     s_std = np.std(np.array(s), 0)
     return s_mu, s_std

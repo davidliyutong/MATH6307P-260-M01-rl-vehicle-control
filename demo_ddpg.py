d4622d3b6a36888322f98945ea063e30a2bc4f65
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2022-01-06 23:32:25 +0800

    Modify demo_ddpg.py - add train loop

--- demo_ddpg.py
+++ demo_ddpg.py
@@ -1,23 +1,22 @@
-
+import torch
+import tqdm
+from torch.utils.tensorboard import SummaryWriter
 from algorithm import DDPG
 from algorithm.utils import agg_double_list
+from simulator import Environment, Vehicle, Park, resetEnvEval, resetEnvVel
+from network import ActorNetwork, CriticNetwork
+import pickle
 
-import gym
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-MAX_EPISODES = 5000
-EPISODES_BEFORE_TRAIN = 100
+MAX_EPISODES = 10000
+EPISODES_BEFORE_TRAIN = 256
 EVAL_EPISODES = 10
 EVAL_INTERVAL = 100
 
 # max steps in each episode, prevent from running too long
-MAX_STEPS = 10000 # None
+MAX_STEPS = 3000  # None
 
-MEMORY_CAPACITY = 10000
-BATCH_SIZE = 100
+MEMORY_CAPACITY = 1000000
+BATCH_SIZE = 128
 CRITIC_LOSS = "mse"
 MAX_GRAD_NORM = None
 
@@ -34,16 +33,36 @@ DONE_PENALTY = None
 
 RANDOM_SEED = 2021
 
-def run(env_id="Pendulum-v0"):
+USE_CUDA = False
+DEVICE = torch.device("cuda:0") if USE_CUDA else torch.device("cpu")
 
-    env = gym.make(env_id)
+SAVE_INTERVAL = 1000
+
+LOAD_STATE_DICT = False
+
+
+def run():
+    env = Environment(Vehicle(0, 0, 0, 0, 0), Park(), reset_fn=resetEnvVel, maxSteps=MAX_STEPS, device=DEVICE)
     env.seed(RANDOM_SEED)
-    env_eval = gym.make(env_id)
+    env.reset()
+
+    env_eval = Environment(Vehicle(0, 0, 0, 0, 0), Park(), reset_fn=resetEnvEval, maxSteps=MAX_STEPS, device=DEVICE)
     env_eval.seed(RANDOM_SEED)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
+    env_eval.reset()
+
+    state_dim = env.observation_space
+    action_dim = env.action_space
+
+    actor_network = ActorNetwork(state_dim, action_dim, device=DEVICE).to(DEVICE)
+    critic_network = CriticNetwork(state_dim, action_dim, device=DEVICE).to(DEVICE)
+    if LOAD_STATE_DICT:
+        actor_network.load_state_dict(torch.load('./checkpoint/actor_network.pth'))
+        critic_network.load_state_dict(torch.load('./checkpoint/actor_network.pth'))
+
+    writer: SummaryWriter = SummaryWriter(log_dir='./log')
 
     ddpg = DDPG(env=env, memory_capacity=MEMORY_CAPACITY,
+                actor_network=actor_network, critic_network=critic_network,
                 state_dim=state_dim, action_dim=action_dim,
                 batch_size=BATCH_SIZE, max_steps=MAX_STEPS,
                 done_penalty=DONE_PENALTY,
@@ -51,37 +70,42 @@ def run(env_id="Pendulum-v0"):
                 reward_gamma=REWARD_DISCOUNTED_GAMMA, critic_loss=CRITIC_LOSS,
                 epsilon_start=EPSILON_START, epsilon_end=EPSILON_END,
                 epsilon_decay=EPSILON_DECAY, max_grad_norm=MAX_GRAD_NORM,
-                episodes_before_train=EPISODES_BEFORE_TRAIN)
+                episodes_before_train=EPISODES_BEFORE_TRAIN, use_cuda=USE_CUDA)
 
-    episodes =[]
-    eval_rewards =[]
-    while ddpg.n_episodes < MAX_EPISODES:
-        ddpg.interact()
-        if ddpg.n_episodes >= EPISODES_BEFORE_TRAIN:
-            ddpg.train()
-        if ddpg.episode_done and ((ddpg.n_episodes+1)%EVAL_INTERVAL == 0):
-            rewards, _ = ddpg.evaluation(env_eval, EVAL_EPISODES)
-            rewards_mu, rewards_std = agg_double_list(rewards)
-            print("Episode: %d, Average Reward: %.5f" % (ddpg.n_episodes+1, rewards_mu))
-            episodes.append(ddpg.n_episodes+1)
-            eval_rewards.append(rewards_mu)
+    episodes = []
+    eval_rewards = []
+    fig, ax = env.InitFrame()
+    with tqdm.tqdm(range(MAX_EPISODES)) as pbar:
+        while ddpg.n_episodes < MAX_EPISODES:
+            ddpg.interact()
+            # env.render(fig, ax, mode='train')
+            if ddpg.n_episodes >= EPISODES_BEFORE_TRAIN:
+                ddpg.train()
+            if ddpg.episode_done and ((ddpg.n_episodes + 1) % EVAL_INTERVAL == 0):
+                rewards, _ = ddpg.evaluation(env_eval, EVAL_EPISODES)
+                rewards_mu, rewards_std = agg_double_list(rewards)
+                print("\nEpisode: %d, Average Reward: %.5f" % (ddpg.n_episodes + 1, rewards_mu))
+                episodes.append(ddpg.n_episodes + 1)
+                eval_rewards.append(rewards_mu)
+                writer.add_scalar('rewards_mu_eval', rewards_mu, ddpg.n_episodes + 1)
+                writer.add_scalar('rewards_std_eval', rewards_std, ddpg.n_episodes + 1)
+            if ddpg.episode_done:
+                writer.add_scalar('n_steps', ddpg.n_steps, ddpg.n_episodes + 1)
+                writer.add_scalar('reward', ddpg.env.reward, ddpg.n_episodes + 1)
+                pbar.update()
 
-    episodes = np.array(episodes)
-    eval_rewards = np.array(eval_rewards)
-    np.savetxt("./output/%s_ddpg_episodes.txt"%env_id, episodes)
-    np.savetxt("./output/%s_ddpg_eval_rewards.txt"%env_id, eval_rewards)
-
-    plt.figure()
-    plt.plot(episodes, eval_rewards)
-    plt.title("%s" % env_id)
-    plt.xlabel("Episode")
-    plt.ylabel("Average Reward")
-    plt.legend(["DDPG"])
-    plt.savefig("./output/%s_ddpg.png"%env_id)
+            if ((ddpg.n_episodes + 1) % SAVE_INTERVAL == 0):
+                torch.save(actor_network.state_dict(), f"./checkpoint/actor_network_{ddpg.n_episodes}.pth")
+                torch.save(critic_network.state_dict(), f"./checkpoint/critic_network_{ddpg.n_episodes}.pth")
+                with open(f"./checkpoint/ddpg_{ddpg.n_episodes}.pth") as f:
+                    pickle.dump(ddpg, f)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 2:
-        run(sys.argv[1])
-    else:
-        run()
+    import os
+
+    print("Start training with resetEnvEval")
+    os.makedirs('./output') if not os.path.exists('./output') else 0
+    os.makedirs('./log') if not os.path.exists('./log') else 0
+    os.makedirs('./checkpoint') if not os.path.exists('./checkpoint') else 0
+    run()
